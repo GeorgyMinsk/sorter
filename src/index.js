5b36c9621861f1dc0c704409a90b7aadb302411f
Georhi Buklaga <buklaga.98@yandex.ru>
2018-09-12 09:43:11 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
-
-  add(element) {
-    // your implementation
-  }
-
-  at(index) {
-    // your implementation
-  }
-
-  get length() {
-    // your implementation
-  }
-
-  toArray() {
-    // your implementation
-  }
-
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
-  }
+constructor() {
+// your implementation
+this.arr = [];
+this.comparator = (left, right) => left - right;
 }
-
+add(element) {
+// your implementation
+this.arr.push(element);
+}
+at(index) {
+// your implementation
+return this.arr[index];
+}
+get length() {
+// your implementation
+return this.arr.length;
+}
+toArray() {
+// your implementation
+return this.arr;
+}
+sort(indices) {
+// your implementation
+var myArr = [];
+for(var i = 0; i < indices.length; i++) {
+myArr.push(this.arr[indices[i]]);
+}
+indices.sort();
+myArr.sort(this.comparator);
+for (var j = 0; j < indices.length; j++){
+this.arr[indices[j]] = myArr[j];
+}
+}
+setComparator(compareFunction) {
+// your implementation
+this.comparator = compareFunction;
+}
+} 
 module.exports = Sorter;
